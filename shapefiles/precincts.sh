ca574b08d3fbd274067076fd59098ca8c51829d1
Forest Gregg <fgregg@uchicago.edu>
2013-10-04 08:54:06 -0500

    reduce precision

--- shapefiles/precincts.sh
+++ shapefiles/precincts.sh
@@ -1,3 +1,4 @@
 wget https://data.cityofchicago.org/download/sgsc-bb4n/application/zip
 unzip dsv15pWE6oiZTEMkQEowJ5f4bmF7zvlR_Qh53s1Ub7A\?filename\=WardPrecincts.zip 
-ogr2ogr -f "GeoJSON" ward_precints.json WardPrecincts.shp -t_srs EPSG:4269
+export OGR_WKT_PRECISION=5
+ogr2ogr -f "GeoJSON" -lco COORDINATE_PRECISION=5 ward_precincts.geojson WardPrecincts.shp -t_srs EPSG:4269
