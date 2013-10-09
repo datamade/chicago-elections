b906eda6b5e872cc9065a85afa1ebebcc6e1f8e0
Eric van Zanten <eric@bahai.us>
2013-10-09 09:16:48 -0500

    Added a couple example queries. Added ForeignKey relationships back in the correct way

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ db = SQLAlchemy(app)
 class Election(db.Model):
     election_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
-    
+    races = db.relationship('Race', backref='election', lazy='dynamic')
+
     def __repr__(self):
         return '<Election %r>' % self.name 
 
@@ -17,7 +18,8 @@ class Race(db.Model):
     race_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     election_id = db.Column(db.Integer, db.ForeignKey('election.election_id'))
-    
+    results = db.relationship('Result', backref='race', lazy='dynamic')
+
     def __repr__(self):
         return '<Race %r>' % self.name 
 
@@ -30,6 +32,11 @@ class Result(db.Model):
     race_id = db.Column(db.Integer, db.ForeignKey('race.race_id'))
     
     def __repr__(self):
-        return '<Result %r>' % self.result_id
+        return '<Result %r %r %r>' % (self.race.election.name, self.race.name, self.option)
 
+# Return number of votes cast for a given option in a given race
+# it = db.session.query(Result, func.sum(Result.votes)).filter(Result.race_id==race.race_id).group_by(Result.option)
+
+# Return the number of votes cast in all races for options like "%MOORE%"
+# it = db.session.query(Result, func.sum(Result.votes)).join(Race).filter(Result.option.like("%MOORE%")).group_by(Race)
 
