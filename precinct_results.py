8205e0fc6bf77dabf23ced7bfbfa610e93d05890
Eric van Zanten <eric@bahai.us>
2013-10-10 17:14:13 -0500

    Modified scraper to look for existing results before making them. Closes #9

--- precinct_results.py
+++ precinct_results.py
@@ -3,13 +3,14 @@ from BeautifulSoup import BeautifulSoup
 import json
 import sqlite3
 import re
+from urlparse import urlparse, parse_qs
 
 conn = sqlite3.connect('results.db', timeout=10)
 c = conn.cursor()
 
-c.execute("DROP TABLE IF EXISTS election")
-c.execute("DROP TABLE IF EXISTS race")
-c.execute("DROP TABLE IF EXISTS result")
+#c.execute("DROP TABLE IF EXISTS election")
+#c.execute("DROP TABLE IF EXISTS race")
+#c.execute("DROP TABLE IF EXISTS result")
 
 c.execute('''CREATE TABLE IF NOT EXISTS election
              (election_id integer PRIMARY KEY, name TEXT)
@@ -33,15 +34,18 @@ s.cache_write_only = False
 with open('precinct_pages.json') as infile :
     precinct_pages = json.loads(infile.read())
 
-def parseTable(table) :
-    for row in results_table.findAll('tr') :
+def parseTable(results_table) :
+    for row in results_table.findAll('tr'):
         row_list = []
-        for cell in row.findAll('td') :
-            if cell.text == '--' :
-                break
+        cells = row.findAll('td')
+        if len(cells) < 2:
+            continue
+        for cell in cells:
             row_list.append(cell.text)
-        if row_list :
+        if len(row_list) > 2:
             yield row_list[::2]
+        else:
+            yield row_list
 
 
 
@@ -54,19 +58,28 @@ for election, races in precinct_pages.items() :
     else :
         c.execute("INSERT INTO election (name) VALUES (?)", (election,))
         election_id = c.lastrowid
-    for race, pages in races.items() :
-        c.execute("INSERT INTO race (election_id, name) VALUES (?, ?)", 
-                  (election_id, race))
-        race_id = c.lastrowid
+    for race, pages in races.items():
+        c.execute("SELECT race_id from race WHERE name = ? AND election_id = ?", (race, election_id))
+        race_id = c.fetchone()
+        if race_id:
+            race_id = race_id[0]
+        else:
+            c.execute("INSERT INTO race (election_id, name) VALUES (?, ?)", 
+                      (election_id, race))
+            race_id = c.lastrowid
         for page in pages :
             print page
-            ward = re.search('.*Ward=(.+?)&', page).group(1)
-            print ward
+            ward = parse_qs(urlparse(page).query)['Ward'][0]
+            c.execute('select result_id from result where race_id = ? and ward = ?', (race_id, ward))
+            result_rows = c.fetchone()
+            if result_rows:
+                continue
             soup = BeautifulSoup(s.urlopen(page))
             results_table = soup.find('table')
             table = parseTable(results_table) 
             try: 
-                header = table.next()[1:]
+                header = table.next()
+                header = header[1:]
             except AttributeError :
                 continue #logging
 
@@ -75,10 +88,12 @@ for election, races in precinct_pages.items() :
                 try :
                     precinct = results.pop(0)
                     int(precinct) # if we can't cast this to int then raise error
-                    for result in zip(header, results) :
+                    for result in zip(header, results):
                         c.execute("INSERT INTO result (race_id, ward, precinct, option, votes) VALUES (?, ?, ?, ?, ?)",
                                   [race_id] + [ward] + [precinct] + list(result))
-                except ValueError :
+
+                except ValueError, e:
+                    print e
                     continue
             conn.commit()
 
