9bada2b9fe025e44ec66ab2278f2d8f679d00521
Eric van Zanten <eric@bahai.us>
2013-11-21 12:02:32 -0600

    Moving the clout endpoint over to the war chest app

--- app.py
+++ app.py
@@ -169,12 +169,6 @@ def war_chest():
     resp.headers['Content-Type'] = 'application/json'
     return resp
 
-@app.route('/clout/')
-def clout():
-    resp = make_response(open('clout.json').read())
-    resp.headers['Content-Type'] = 'application/json'
-    return resp
-
 @app.route('/aldermen/')
 def aldermen():
     resp = make_response(open('aldermen.json').read())
