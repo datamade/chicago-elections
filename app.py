7e0a13148fc9f0cb999364fd589e75fd041bc554
Eric van Zanten <eric@bahai.us>
2013-12-08 11:51:17 -0600

    Endpoints looking pretty OK.

--- app.py
+++ app.py
@@ -4,45 +4,32 @@ from bson import json_util
 import json
 from operator import itemgetter
 from itertools import groupby
+from datetime import date, datetime
 
 app = Flask(__name__)
 
 c = pymongo.MongoClient()
 db = c['openelex']
 
+dhandler = lambda obj: obj.isoformat() if isinstance(obj, date) or isinstance(obj, datetime) else None
+
 @app.route('/elections/')
 def elections():
-    # Returns all elections
+    # Displays elections by ID and contests in each election
     contests = list(db['contest'].find())
     resp = []
-    for contest in contests:
-        d = {
-            'election_id': contest['election_id'],
-            'office_name': contest['raw_office'],
-            'division': 'ocd-division/country:us/state:il/place:chicago',
-            'results': [],
-        }
-        print 'Contest: %s' % contest['raw_office']
-      # for ward in range(1,51):
-      #     d['ward'] = ward
-      #     query = {
-      #         'election_id': contest['election_id'],
-      #         'ocd_id': {'$regex': r'ward:%s/precinct' % ward }
-      #     }
-      #     results = list(db['result'].find(query))
-      #     results = sorted(results, key=itemgetter('candidate_slug'))
-      #     print 'WARD: %s' % ward
-      #     for k,g in groupby(results, key=itemgetter('candidate_slug')):
-      #         print db['candidate'].find_one({'slug':k})['raw_full_name']
-      # print '*'*100
-        resp.append(d)
-    resp = make_response(json_util.dumps(resp))
+    elections = sorted(contests, key=itemgetter('election_id'))
+    for k,g in groupby(elections, key=itemgetter('election_id')):
+        conts = [c['raw_office'] for c in list(g)]
+        resp.append({k:conts})
+    resp = make_response(json.dumps(resp, default=dhandler))
     resp.headers['Content-Type'] = 'application/json'
     return resp
 
 @app.route('/elections/<election_id>/')
 def elections_by_id(election_id):
-    # Returns a single election
+    # Returns a single election and the results of the contests
+    # in that election by ward
     resp = []
     for ward in range(1,51):
         d = {}
