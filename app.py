3337c40dbf333d8babdac3013bcf8b65551ef4d1
Eric van Zanten <eric@bahai.us>
2013-12-08 11:37:32 -0600

    Got the openelections backed endpoint looking pretty OK for individual elections

--- app.py
+++ app.py
@@ -43,29 +43,53 @@ def elections():
 @app.route('/elections/<election_id>/')
 def elections_by_id(election_id):
     # Returns a single election
-    contests = db['contest'].find({'election_id': election_id})
     resp = []
-    for contest in contests:
-        d = {
-            'election_id': contest['election_id'],
-            'office_name': contest['raw_office'],
-            'division': 'ocd-division/country:us/state:il/place:chicago',
-            'results': [],
-        }
-        print 'Contest: %s' % contest['raw_office']
-        for ward in range(1,51):
-            d['ward'] = ward
-            query = {
-                'election_id': contest['election_id'],
-                'ocd_id': {'$regex': r'ward:%s/precinct' % ward }
+    for ward in range(1,51):
+        d = {}
+        d['ward'] = ward
+        pipeline = [{
+            '$match': {
+                'election_id': election_id, 
+                'ocd_id': {
+                    '$regex': r'ward:%s/precinct' % ward
+                }
+            }}, {
+                '$group': {
+                    '_id': '$candidate_slug', 
+                    'votes': {
+                        '$sum': '$total_votes'
+                    },
+                    'voters': {
+                        '$sum': '$registered_voters'
+                    },
+                    'cand_id': {
+                        '$addToSet': '$candidate'
+                    },
+                }
             }
-            results = list(db['result'].find(query))
-            if results:
-                d['results'] = sorted(results, key=itemgetter('candidate_slug'))
-               # print 'WARD: %s' % ward
-               # for k,g in groupby(results, key=itemgetter('candidate_slug')):
-               #     print db['candidate'].find_one({'slug':k})['raw_full_name']
-        print '*'*100
+        ]
+        results = db['result'].aggregate(pipeline)['result']
+        if results:
+            d['results'] = []
+            votes = [r['votes'] for r in results]
+            d['votes_cast'] = sum(votes)
+            winning = max(votes)
+            d['election_id'] = election_id
+            d['division'] = 'ocd-division/country:us/state:il/place:chicago'
+            d['office_name'] = 'Alderman Ward %s' % ward
+            for result in results:
+                res = {}
+                cand = db.candidate.find_one(result['cand_id'][0])
+                res['votes'] = result['votes']
+                res['voters'] = result['voters']
+                if res['votes'] == winning:
+                    res['winner'] = True
+                else:
+                    res['winner'] = False
+                res['name'] = cand['raw_full_name']
+                percent = (float(res['votes']) / float(d['votes_cast'])) * 100
+                res['percent'] = '%(percent).2f' % {'percent':percent}
+                d['results'].append(res)
         resp.append(d)
     resp = make_response(json_util.dumps(resp))
     resp.headers['Content-Type'] = 'application/json'
